771be0c9c0bea3d0829e834eae9d0d273487789a
Ian <heratyian@gmail.com>
2023-02-13 19:53:39 +0000

    Add form to calculate payment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,22 @@ class ApplicationController < ActionController::Base
     @square_root = Math.sqrt(@number)
     render({ template: "calculation_templates/square_root_results" })
   end
+
+  def blank_payment_form
+    render({ template: "calculation_templates/payment_form" })
+  end
+
+  def payment_results
+    @apr = params.fetch("apr").to_f
+    @years = params.fetch("years").to_f
+    @principal = params.fetch("principal").to_f
+
+    n = @years * 12
+    r = @apr / 100 / 12
+    @numerator = r * @principal
+    @denominator = 1 - (1 + r) ** -n
+    @payment = @numerator / @denominator
+
+    render({ template: "calculation_templates/payment_results" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get("square_root/new", { controller: "application", action: "blank_square_root_form" })
   get("square_root/results", { controller: "application", action: "square_root_results" })
 
+  get("payment/new", { controller: "application", action: "blank_payment_form" })
+  get("payment/results", { controller: "application", action: "payment_results" })
+
   root({ controller: "application", action: "blank_square_form" })
 end
