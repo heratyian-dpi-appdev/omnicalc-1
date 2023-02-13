a7f3178a2f00fd0bae6351337b6858d10aebe9fc
Ian <heratyian@gmail.com>
2023-02-13 18:11:29 +0000

    Add form to calculate square of user input

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  def blank_square_form
+    render({ template: "calculation_templates/square_form" })
+  end
 
+  def square_results
+    @number = params.fetch("number").to_f
+    @square = @number ** 2
+    render({ template: "calculation_templates/square_results" })
+  end
 end
